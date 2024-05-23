31e97773a26f5600b3904c2967771a99f55ba660
oribal23daw2 <15586216.clot@fje.edu>
2024-05-23 14:41:09 +0200

    Acabat

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'ahorcado'
+    database: 'ahorcado',
 });
 
 connection.connect(err => {
